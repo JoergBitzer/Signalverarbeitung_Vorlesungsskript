d83da9b150af255236aa66890aca2f5a23ed3a3b
Maximilian Dechert <maximilian.dechert@student.jade-hs.de>
2021-10-08 13:34:55 +0200

    HP/LP/... hinzugefügt

--- legacy/PicGenerierungPython/Kapitel_3456/Figure_6_3.py
+++ legacy/PicGenerierungPython/Kapitel_3456/Figure_6_3.py
@@ -10,7 +10,7 @@ fg = [1000, 3000]# cutoff frequency
 order = 6
 x = [0.0]*Fs
 x[0] = 1.0
-b, a = scipy.signal.butter(N=order, Wn=fg, btype='bandpass', output='ba', fs=Fs)
+b,a = scipy.signal.butter(N=order, Wn=fg, btype='bandpass', output='ba', fs=Fs)
 y = scipy.signal.lfilter(b, a, x)
 spectrum = numpy.fft.fft(y)
 # dB, max amplitude = 0dB
@@ -20,10 +20,14 @@ freq_vec = numpy.linspace(0, Fs, len(spectrum_abs)) # freq bins, for plotting
 fig, ax_spectrum = pyplot.subplots()
 
 # ideal frequency respose of filter
-ax_spectrum.step(y=[-100, -100, 0, -100, -100, 0, -100], x=[0, fg[0], fg[1], Fs/2, Fs-fg[1], Fs-fg[0], Fs], label="Optimale Entwurfsvorgabe")
+ax_spectrum.step(y=[-100, -100, 0, -100, -100, 0, -100], 
+        x=[0, fg[0], fg[1], Fs/2, Fs-fg[1], Fs-fg[0], Fs], 
+        label="Optimale Entwurfsvorgabe")
 # actual realization
-ax_spectrum.plot(freq_vec, spectrum_abs, color='r', label="Mögliche Realisierung")
-ax_spectrum.set(xlabel='Frequenz in Hz ', ylabel='Dämpfung in dB', xlim=[0, Fs/2], ylim=[-105, 5], title=f'Bandpass {order}.Odnung')
+ax_spectrum.plot(freq_vec, spectrum_abs, 
+        color='r', label="Mögliche Realisierung")
+ax_spectrum.set(xlabel='Frequenz in Hz ', ylabel='Dämpfung in dB', 
+        xlim=[0, Fs/2], ylim=[-105, 5], title=f'Bandpass {order}.Odnung')
 pyplot.legend()
 
 
